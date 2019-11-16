6052b62a983f95a7c94b8681f6832f89affc0b8e
andrearose26 <andreaa.lacson@gmail.com>
2019-11-16 16:10:26 -0500

    testing branches :v:

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //SET UP MAIN GAMEPLAY PAGE ==========================
-let score = [];
+let score = []; //only doors and score REALLY needed
 
 const doors = [".door0", ".door1", ".door2", ".door3"];
 
@@ -35,95 +35,85 @@ const characters = {
     ]
 }
 
-
+//When document starts, do the following: 
 $(document).ready(function () {
-    $('#game').hide();
-    //When spacebar is pressed, 
-    //- hide start page
-    //- show game page
-    //- activate s key
-    //- activate l key
-    $(document).on("keydown", function (keypress) {
+    $('#game').hide(); //Hide the game div
+    $(document).on("keydown", function (keypress) { //When the spacebar is pressed
         if (keypress.keyCode === 32) {
-            $('#start').hide();
-            $('#game').show();
-            keyFunction();
-            winOrLose();
+            $('#start').hide(); //- hide start page
+            $('#game').show(); //- show game page
+            keyFunction(); //- activate keys
+            winOrLose(); //- start checking the score
         }
     })
 });
 
-let randomDoorBowser, randomDoorBowsy; //selects a random array number
+//CHARACTER APPEARANCES ================================
 
-//Function makes Bowser appear randomly in a door
-const bowserAppear = function () {
+let randomDoorBowser, randomDoorBowsy; //selects a random door number from the doors array
 
-    setInterval(function () { //calls a random door every second
+const bowserAppear = function () { //Function makes Bowser appear randomly in a door
+
+    setInterval(function () { //calls a random door whenever Bowser appears
         randomDoorBowser = Math.floor(Math.random() * doors.length)
-        // console.log(randomDoorBowser);
     }, 3500);
 
-    if (randomDoorBowser !== randomDoorBowsy){
-        $(".door" + randomDoorBowser).append(`<img src="assets/bowser1.png" class="character bowser" alt="">`); //adds the html of Bowser to DOM
+    if (randomDoorBowser !== randomDoorBowsy){ //if Bowser's door !== Bowsy's door
+        $(".door" + randomDoorBowser).append(`<img src="assets/bowser1.png" class="character bowser" alt="">`); //add the html of Bowser to DOM
     
-        setTimeout(function () { //removes Bowser after 1 second
+        setTimeout(function () { //removes Bowser after a period of time
             $('img').remove();
         }, 800)
     }
 }
 
-const bowsyAppear = function () {
+const bowsyAppear = function () { //Function makes Bowsy appear randomly in a door
 
-    setInterval(function () { //calls a random door every second
+    setInterval(function () { //calls a random door whenever Bowsy appears
         randomDoorBowsy = Math.floor(Math.random() * doors.length)
-        // console.log(randomDoorBowsy);
     }, 1500);
 
-    if(randomDoorBowsy !== randomDoorBowser){
+    if (randomDoorBowsy !== randomDoorBowser) { //if Bowsy's door !== Bowsers's door
         $(".door" + randomDoorBowsy).append(`<img src="assets/bowser0.png" class="character bowsy" alt="">`); //adds the html of Bowsy to DOM
-        setTimeout(function () { //removes Bowsy after 1 second
+        setTimeout(function () { //removes Bowsy after a period of time
             $('img').remove();
         }, 900)
     }
 
 }
-const bowserAppears = setInterval(bowserAppear, 3500); //Bowser will appear every 2.5 seconds
-const bowsyAppears = setInterval(bowsyAppear, 1500); //Bowsy will appear every 2.5 seconds
 
+const bowserAppears = setInterval(bowserAppear, 3500); //Calls functions at diferent interval
+const bowsyAppears = setInterval(bowsyAppear, 1500); 
 
 //SET UP USER CONTROLS ===============================
 
-let bowserLocation; //equal to door number
+let bowserLocation; //equal to door number where user presses
 
 const keyFunction = function () {
     $(document).on("keydown", function (keypress) {
         //S Key
         if (keypress.keyCode === 83) {
-            //add a class of doorSelected to show that it was selected
-            $(".key83").addClass('doorSelected');
+            $(".key83").addClass('doorSelected'); //add a class of doorSelected to show that it was selected 
             bowserLocation = 0;
         }
         //D Key
         if (keypress.keyCode === 68) {
-            //add a class of doorSelected to show that it was selected
-            $(".key68").addClass('doorSelected');
+            $(".key68").addClass('doorSelected'); //add a class of doorSelected to show that it was selected
             bowserLocation = 1;
         }
         //K Key
         if (keypress.keyCode === 75) {
-            //add a class of doorSelected to show that it was selected
-            $(".key75").addClass('doorSelected');
+            $(".key75").addClass('doorSelected'); //add a class of doorSelected to show that it was selected
             bowserLocation = 2;
         }
         //L Key
         if (keypress.keyCode === 76) {
-            //add a class of doorSelected to show that it was selected
-            $(".key76").addClass('doorSelected');
+            $(".key76").addClass('doorSelected'); //add a class of doorSelected to show that it was selected
             bowserLocation = 3;
         }
     })
-    //removes class of doorSelected when user lifts keys
-    $(document).keyup(function () {
+
+    $(document).keyup(function () { //removes class of doorSelected when user lifts keys
         $('.key83').removeClass('doorSelected');
         $('.key68').removeClass('doorSelected');
         $('.key75').removeClass('doorSelected');
@@ -131,19 +121,17 @@ const keyFunction = function () {
     });
 }
 
-
-
 //SET UP SCORING SYSTEM ==============================
 
-score = [0];
+score = [0]; 
 let newScore, key76, key83, key68, key75;
 
-function bowserInDoor() {
+function bowserInDoor() { //checks if any Bowser is in the door
     let locationValue = $('.door' + bowserLocation).find('img').val();
-    if (locationValue === undefined) { //Someone is not there and correct key is selected
+    if (locationValue === undefined) { //If someone is not there and correct key is selected
         score--;
         $('audio').get(1).play();
-    } else if (locationValue === '') { //Someone is there and correct key is selected
+    } else if (locationValue === '') { //If someone is there and correct key is selected
         // score++;
         whichBowser();
         bowserPoints();
@@ -157,8 +145,6 @@ let bowser, bowsy; //determines bowser's location
 const whichBowser = function () {
     bowser = $('.bowser').length;
     bowsy = $('.bowsy').length;
-    console.log(bowser);
-    console.log(bowsy)
 }
 
 
@@ -166,11 +152,9 @@ const whichBowser = function () {
 const bowserPoints = function () {
     if (bowser === 1){ //bowser is there
         score += 2;
-        console.log('bowser is there');
     }
     if(bowsy === 1) {
         score++;
-        console.log('bowsy is there');
     }
 }
 
